7e02223bfe53c66e1037928a8f1fbb4c2e1fb92f
Developer <user@example.com>
2026-03-31 15:07:29 +0000

    chore: 配置云开发环境ID

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
-        env: 'your-cloud-env-id', // 请替换为你的云开发环境ID
+        env: 'cloud1-0gfe5z1qae319fb6',
         traceUser: true,
       });
     }
